7f13f5ae6658fe040bf116b1c536fffcbba8e926
therubick <mohamed_ayman_07@hotmail.com>
2021-06-09 22:40:24 +0200

    fixing testcases script

--- testCasesScript.sh
+++ testCasesScript.sh
@@ -1,4 +1,6 @@
+mkdir testcasesResults
 for i in $(ls testcases)
 do
-./a.out < ./testcases/$i && cp resultsFile.txt testcases/{$i}Results.txt
+./a.out < ./testcases/$i && cp resultsFile.txt testcasesResults/${i}Results.txt && \
+./a.out < ./testcases/$i | grep "error" >> testcasesResults/${i}Errors.txt
 done
